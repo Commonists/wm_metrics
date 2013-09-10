122ea0ed227e8d771bceca4829cf549ffeffd9de
Jean-Frédéric <JeanFred@github>
2013-09-10 11:37:34 +0200

    Reindent code to use spaces rather than tabs

--- new_editor.py
+++ new_editor.py
@@ -3,37 +3,38 @@
 import json
 
 def new_editors(old_period, new_period):
-	# new editors list
-	new_editors = []
-	# opening old period json file
-	f_old		= open(old_period)
-	data_old	= json.load(f_old)["result"]["Individual Results"][0]
-	# iteration on each user of individual results list
-	#	counts editor without edit on old period and add them to new_editors
-	for k in data_old.keys():
-		if(int(data_old[k]["edits"])==0):
-			new_editors.append(k)
-	f_old.close()
+    # new editors list
+    new_editors = []
+    # opening old period json file
+    f_old           = open(old_period)
+    data_old        = json.load(f_old)["result"]["Individual Results"][0]
+    # iteration on each user of individual results list
+    #       counts editor without edit on old period and add them to new_editors
+    for k in data_old.keys():
+        if(int(data_old[k]["edits"])==0):
+            new_editors.append(k)
+    f_old.close()
 
-	f_new	= open(new_period)
-	data_new= json.load(f_new)["result"]["Individual Results"][0]
-	# new editors that survived
-	count_new = 0
-	# new editors that survived with more than 1 edit per month
-	count_one_epm = 0
-	# new editors that survived with more than 10 edits per month
-	count_ten_epm = 0
-	max_edit_new = 0
-	for k in new_editors:
-		edits = int(data_new[k]["edits"])
-		max_edit_new = max(max_edit_new, edits)
-		if(edits>=1):
-			count_new = count_new + 1
-		if(edits>=6):
-			count_one_epm = count_one_epm + 1
-		if(edits>=60):
-			count_ten_epm = count_ten_epm + 1
-	print "New editors that kept editing after WLM2011(Fr): " + str(count_new)
-	print "\t" + str(count_one_epm) + " with more than one edit per month (6 months period)"
-	print "\t" + str(count_ten_epm) + " with more than ten edits per month (6 months period)"
-	print "\t" + str(max_edit_new) + " max edits for a new contributors (6 months period)"
+    f_new   = open(new_period)
+    data_new= json.load(f_new)["result"]["Individual Results"][0]
+    # new editors that survived
+    count_new = 0
+    # new editors that survived with more than 1 edit per month
+    count_one_epm = 0
+    # new editors that survived with more than 10 edits per month
+    count_ten_epm = 0
+    max_edit_new = 0
+    for k in new_editors:
+        edits = int(data_new[k]["edits"])
+        max_edit_new = max(max_edit_new, edits)
+        if(edits>=1):
+            count_new = count_new + 1
+        if(edits>=6):
+            count_one_epm = count_one_epm + 1
+        if(edits>=60):
+            count_ten_epm = count_ten_epm + 1
+    print "New editors that kept editing after WLM2011(Fr): " + str(count_new)
+    print "\t" + str(count_one_epm) + " with more than one edit per month (6 months period)"
+    print "\t" + str(count_ten_epm) + " with more than ten edits per month (6 months period)"
+    print "\t" + str(max_edit_new) + " max edits for a new contributors (6 months period)"
+
