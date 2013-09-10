a520b8575e8ff734fac73a745a8070d27dc33e60
PierreSelim <ps.huard@gmail.com>
2013-09-11 01:43:37 +0200

    introducing levels
    function new_editors now takes levels as thrid arguments
    I should add this as optional argument from command lines soon.

--- new_editor.py
+++ new_editor.py
@@ -2,39 +2,36 @@
 
 import json
 
-def new_editors(old_period, new_period):
+DEFAULT_LEVELS="1, 6, 60"
+
+def new_editors(old_period, new_period, levels=DEFAULT_LEVELS):
     # new editors list
     new_editors = []
     # Loading old period json filehandle
-    data_old        = json.load(old_period)["result"]["Individual Results"][0]
+    json_old = json.load(old_period)
+    data_old = json_old["result"]["Individual Results"][0]
     # iteration on each user of individual results list
     #       counts editor without edit on old period and add them to new_editors
     for k in data_old.keys():
         if(int(data_old[k]["edits"])==0):
             new_editors.append(k)
 
-    data_new= json.load(new_period)["result"]["Individual Results"][0]
-    # new editors that survived
-    count_new = 0
-    # new editors that survived with more than 1 edit per month
-    count_one_epm = 0
-    # new editors that survived with more than 10 edits per month
-    count_ten_epm = 0
-    max_edit_new = 0
+    json_new= json.load(new_period)
+    data_new= json_new["result"]["Individual Results"][0]
+    level = levels.split(",")
+    # count of surviving editors by level
+    new_counts = dict()
     for k in new_editors:
         edits = int(data_new[k]["edits"])
-        max_edit_new = max(max_edit_new, edits)
-        if(edits>=1):
-            count_new = count_new + 1
-        if(edits>=6):
-            count_one_epm = count_one_epm + 1
-        if(edits>=60):
-            count_ten_epm = count_ten_epm + 1
-    print """New editors that kept editing after WLM2011(Fr): %s
-\t%s with more than one edit per month (6 months period)"
-\t%s with more than ten edits per month (6 months period)"
-\t%s max edits for a new contributors (6 months period)"""\
-    % (count_new, count_one_epm, count_ten_epm, max_edit_new)
+        for l in level:
+            if edits>=int(l):
+                if int(l) in new_counts.keys():
+                    new_counts[int(l)] = new_counts[int(l)]+1
+                else:
+                    new_counts[int(l)] = 1
+    print "%s - %s" % (json_new["parameters"]["start_date"], json_new["parameters"]["end_date"])
+    for k in sorted(new_counts.keys()):
+        print "\tnew editors with more than %d edits:\t %d" % (k, new_counts[k])
 
 
 def getopt_fallback():
