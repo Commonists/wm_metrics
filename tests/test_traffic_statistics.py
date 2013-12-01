88a8c476b86ce5df0f60c53fa17d284192b14fec
Jean-Fred <JeanFred@github>
2013-12-01 23:09:32 +0100

    Add class to retrieve traffic statistics
    As a program evaluator, I want to be be able to retrieve
    Wikipedia article traffic statistics.
    
    - Add module to retrieve article traffic statistics from the
    stats.grok.se service.
    - Add unit test class.

---
+++ tests/test_traffic_statistics.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+"""Unit tests for traffic statistics stuff."""
+
+import unittest
+from traffic_statistics import Traffic
+
+
+class TestTraffic(unittest.TestCase):
+
+    """Test Traffic class."""
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ traffic_statistics.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+
+class Traffic:
+
+    """Wikipedia article statistics."""
+
+    def __init__(self, title, site):
+        self.site = site
+        self.title = title
