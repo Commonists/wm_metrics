d2e5e35b43751806add91e09ce68b606d6bc432b
Jean-Fred <JeanFred@github>
2013-12-01 23:24:42 +0100

    Traffic statistics: add method to build stats.grok.se
    The stats.grok.se web service can provide traffic statistics,
    in JSON format, at an URL constructed using the article title,
    the project, the year and the month.
    
    Add method _make_URL() to make such an URL
    Add unit test for it

--- tests/test_traffic_statistics.py
+++ tests/test_traffic_statistics.py
@@ -11,5 +11,13 @@ class TestTraffic(unittest.TestCase):
 
     """Test Traffic class."""
 
+    def test_make_url(self):
+        """Test _make_url()."""
+        traffic = Traffic('France', 'fr')
+        result = traffic._make_url(2013, 01)
+        expected = 'http://stats.grok.se/json/fr/201301/France'
+        self.assertEqual(result, expected)
+
+
 if __name__ == '__main__':
     unittest.main()
--- traffic_statistics.py
+++ traffic_statistics.py
@@ -9,3 +9,7 @@ class Traffic:
     def __init__(self, title, site):
         self.site = site
         self.title = title
+
+    def _make_url(self, year, month):
+        """Make the URL to the JSON output of stats.grok.se service."""
+        return "http://stats.grok.se/json/{0:s}/{1:d}{2:02d}/{3:s}".format(self.site, year, month, self.title)
