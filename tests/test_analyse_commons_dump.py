19171aa6760bf6363091cc71125289e3f8999794
Jean-Fred <JeanFred@github>
2013-12-07 17:37:14 +0100

    Add test for get_categories_from_text
    This method parses wikitext looking for categories using regexes.
    Add simple test checking no results, one result and two results.

--- tests/test_analyse_commons_dump.py
+++ tests/test_analyse_commons_dump.py
@@ -1,10 +1,21 @@
 """Unit tests for Dump analysing stuff."""
 
 import unittest
+from analyse_commons_dump import get_categories_from_text
 
 
 class TestAnalyseCommonsDump(unittest.TestCase):
-    pass
+
+    def test_get_categories_from_text(self):
+        """Test get_categories_from_text."""
+        values = [
+            ('None', []),
+            ('[[Category:A]]', ['A']),
+            ('[[Category:A]] [[Category:B C]]', ['A', 'B C']),
+        ]
+        for value, expected in values:
+            self.assertEqual(get_categories_from_text(value), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
