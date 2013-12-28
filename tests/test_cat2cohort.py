351ec5ef16a131d69054d05cef7666890a7d8888
Jean-Fred <JeanFred@github>
2013-12-28 19:44:28 +0100

    Add unittests module for cat2cohort
    I want to have unit tests for the methods in cat2cohort.
    
    Add empty module implementing unittest.

---
+++ tests/test_cat2cohort.py
@@ -0,0 +1,11 @@
+"""Unit tests for cat2cohort."""
+
+import unittest
+from cat2cohort import api_url
+
+
+class TestCat2Cohort(unittest.TestCase):
+
+    """Test methods from Cat2Cohort."""
+
+    pass
