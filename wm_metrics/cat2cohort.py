ce37560435fed0b73d21a2de861e368499de67c0
Jean-Fred <JeanFred@github>
2014-01-05 18:38:12 +0100

    Reformat file and fix style & documentation
    Fix all PEP8 and PEP257 violations.
    (the mixed tabs/spaces was particularly evil).

--- wm_metrics/cat2cohort.py
+++ wm_metrics/cat2cohort.py
@@ -1,45 +1,45 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 """
-cat2cohort.py
-	The aim of this script is to allow program leaders to export a category
-	filled with User pages into a WikiMetrics cohort CSV file in order to 
-	perform their evaluation analysis.
+Export a Wiki category into a cohort.
 
-	Test:
-		python cat2cohort.py -l fr -c "Utilisateur participant au projet Afripédia"
+The aim of this script is to allow program leaders to export a category
+filled with User pages into a WikiMetrics cohort CSV file in order to
+perform their evaluation analysis.
+
+Test:
+    python cat2cohort.py -l fr -c "Utilisateur participant au projet Afripédia"
+
 """
-import mw_api, mw_util
+
+import mw_api
+import mw_util
+
 
 def api_url(lang):
-	"""
-	Returns the URL of the API based on the language of Wikipedia.
-	"""
-	return "https://%s.wikipedia.org/w/api.php" % (lang)
-
+    """Return the URL of the API based on the language of Wikipedia."""
+    return "https://%s.wikipedia.org/w/api.php" % lang
 
 
 def list_users(mw, category, lang):
-	"""
-	List users from a wiki category and print lines of the cohort CSV.
-	"""
-	list_query = mw_api.MwApiQuery(properties={
-				"list"   : "categorymembers",
-				"cmtitle": category,
-				"cmprop" : "ids|title|timestamp",
-				"cmtype" : "page",
-				"cmsort" : "sortkey",
-				"cmdir"  : "asc",
-				"cmlimit": "max"
-			})
-	for page in mw.process_query(list_query):
-		if ":" in page['title']:
-			username = page['title'].split(":")[1]
-			yield (username, lang)
+    """List users from a wiki category and print lines of the cohort CSV."""
+    list_query = mw_api.MwApiQuery(properties={
+        "list": "categorymembers",
+        "cmtitle": category,
+        "cmprop": "ids|title|timestamp",
+        "cmtype": "page",
+        "cmsort": "sortkey",
+        "cmdir": "asc",
+        "cmlimit": "max"
+    })
+    for page in mw.process_query(list_query):
+        if ":" in page['title']:
+            username = page['title'].split(":")[1]
+            yield (username, lang)
 
 
 def cat_to_cohort(language, category):
-    """Get the user list from the given category and language."""
+    """Return the CSV cohort from the given category and language."""
     mw = mw_api.MwWiki(url_api=api_url(language))
     user_list = list_users(mw, mw_util.str2cat(category), language)
     csv_text = _userlist_to_CSV_cohort(user_list)
@@ -58,30 +58,29 @@ def _make_CSV_line(username, language):
 
 
 def main():
-	"""
-	Main function of the script cat2cohort.py
-	"""
-	from argparse import ArgumentParser
-	description = "Export a Wiki category into a cohort"
-	parser = ArgumentParser(description=description)
+    """Main function of the script cat2cohort."""
+    from argparse import ArgumentParser
 
-	parser.add_argument("-c", "--category",
-    					type=str,
+    description = "Export a Wiki category into a cohort"
+    parser = ArgumentParser(description=description)
+
+    parser.add_argument("-c", "--category",
+                        type=str,
                         dest="category",
-						metavar="CAT",
-						required=True,
+                        metavar="CAT",
+                        required=True,
                         help="The wiki category to export")
 
-	parser.add_argument("-l", "--lang",
+    parser.add_argument("-l", "--lang",
                         type=str,
                         dest="lang",
                         metavar="LANG",
                         required=True,
                         help="Wiki language")
 
-	args = parser.parse_args()
-	print cat_to_cohort(args.lang, args.category)
+    args = parser.parse_args()
+    print cat_to_cohort(args.lang, args.category)
 
 
 if __name__ == "__main__":
-	main()
+    main()
