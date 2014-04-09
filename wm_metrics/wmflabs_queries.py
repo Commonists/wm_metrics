4480bdd4862a0527121fb6cb171302f5b15fdc65
PierreSelim <pierre-selim.huard@wikimedia.fr>
2014-04-10 00:09:56 +0200

    nb files, nb uploaders, nb featured files

--- wm_metrics/wmflabs_queries.py
+++ wm_metrics/wmflabs_queries.py
@@ -12,7 +12,22 @@ CROSS JOIN page ON image.img_name = page.page_title
 CROSS JOIN categorylinks ON page.page_id = categorylinks.cl_from
 LEFT JOIN oldimage ON image.img_name = oldimage.oi_name AND oldimage.oi_timestamp = (SELECT MIN(o.oi_timestamp) FROM oldimage o WHERE o.oi_name = image.img_name)
 WHERE  
-    categorylinks.cl_to = %s
+    categorylinks.cl_to = '%s'
+    AND IF(oldimage.oi_timestamp IS NULL, img_timestamp, oldimage.oi_timestamp)  BETWEEN %s AND %s
+ORDER BY img_timestamp ASC;""" % (category, t1, t2)
+
+def count_files_in_category(category, t1, t2):
+	"""
+	count_files_in_category
+		List all files in category uploaded between timestamp t1 and t2
+	"""
+	return u"""SELECT /* SLOW_OK */ COUNT(page.page_title) AS IMAGES
+FROM image
+CROSS JOIN page ON image.img_name = page.page_title 
+CROSS JOIN categorylinks ON page.page_id = categorylinks.cl_from
+LEFT JOIN oldimage ON image.img_name = oldimage.oi_name AND oldimage.oi_timestamp = (SELECT MIN(o.oi_timestamp) FROM oldimage o WHERE o.oi_name = image.img_name)
+WHERE  
+    categorylinks.cl_to = '%s'
     AND IF(oldimage.oi_timestamp IS NULL, img_timestamp, oldimage.oi_timestamp)  BETWEEN %s AND %s
 ORDER BY img_timestamp ASC;""" % (category, t1, t2)
 
@@ -27,7 +42,7 @@ CROSS JOIN page ON image.img_name = page.page_title
 CROSS JOIN categorylinks ON page.page_id = categorylinks.cl_from
 LEFT JOIN oldimage ON image.img_name = oldimage.oi_name AND oldimage.oi_timestamp = (SELECT MIN(o.oi_timestamp) FROM oldimage o WHERE o.oi_name = image.img_name)
 WHERE  
-    categorylinks.cl_to = %s
+    categorylinks.cl_to = '%s'
     AND IF(oldimage.oi_timestamp IS NULL, img_timestamp, oldimage.oi_timestamp)  BETWEEN %s AND %s
 ORDER BY img_timestamp ASC;""" % (category, t1, t2)
 
@@ -41,7 +56,7 @@ FROM
 	   CROSS JOIN categorylinks c2 ON page.page_id = c2.cl_from
 	   LEFT JOIN oldimage ON image.img_name = oldimage.oi_name AND oldimage.oi_timestamp = (SELECT MIN(o.oi_timestamp) FROM oldimage o WHERE o.oi_name = image.img_name)
 	   WHERE  
-	       categorylinks.cl_to = %s
+	       categorylinks.cl_to = '%s'
 	      AND IF(oldimage.oi_timestamp is NULL, img_timestamp, oldimage.oi_timestamp)  BETWEEN %s AND %s
 	      AND (c2.cl_to = "Quality_images" OR c2.cl_to = "Valued_images_supported_by_Wikimedia_France" OR c2.cl_to = "Featured_pictures_supported_by_Wikimedia_France")
    GROUP BY page.page_title
