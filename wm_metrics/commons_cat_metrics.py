89d5c90a2fd8f9ae3bfd8fa706e8803ccf1d0007
PierreSelim <pierre-selim.huard@wikimedia.fr>
2014-04-13 11:24:13 +0200

    adding usage of FDC round and quarter to commons cat metrics

--- wm_metrics/commons_cat_metrics.py
+++ wm_metrics/commons_cat_metrics.py
@@ -1,3 +1,4 @@
+import fdc
 import MySQLdb
 import mw_util
 from argparse import ArgumentParser
@@ -8,25 +9,29 @@ T2 = "20140401000000"
 
 class CommonsCatMetrics:
 	"""Wrapper class for the Category Metrics"""
-	def __init__(self, category):
+	def __init__(self, category, round_fdc, q):
 		self.catname = category
 		self.catsql = category.replace(" ", "_")
 		self.db = MySQLdb.connect(host="commonswiki.labsdb", db="commonswiki_p", read_default_file="~/replica.my.cnf")
 		self.cursor = self.db.cursor()
+		# round fdc
+		timestamps = round_fdc.quarter(q)
+		self.timestamp1 = timestamps['start']
+		self.timestamp2 = timestamps['end']
 
 	def get_nb_uploaders(self, timestamp1, timestamp2):
 		"""Amount of uploaders on the period"""
-		query = wmflabs_queries.count_uploaders_in_category(self.catsql, timestamp1, timestamp2)
+		query = wmflabs_queries.count_uploaders_in_category(self.catsql, self.timestamp1, self.timestamp2)
 		self.cursor.execute(query)
 		return long(self.cursor.fetchone()[0])
 
 	def get_nb_files(self, timestamp1, timestamp2):
-		query = wmflabs_queries.count_files_in_category(self.catsql, timestamp1, timestamp2)
+		query = wmflabs_queries.count_files_in_category(self.catsql, self.timestamp1, self.timestamp2)
 		self.cursor.execute(query)
 		return long(self.cursor.fetchone()[0])
 
 	def get_nb_featured_files(self, timestamp1, timestamp2):
-		query = wmflabs_queries.count_featured_files_in_category(self.catsql, timestamp1, timestamp2)
+		query = wmflabs_queries.count_featured_files_in_category(self.catsql, self.timestamp1, self.timestamp2)
 		self.cursor.execute(query)
 		return long(self.cursor.fetchone()[0])
 
@@ -49,8 +54,28 @@ def main():
                         metavar="CAT",
                         required=True,
                         help="The Commons category without Category:")
+	parser.add_argument("-y", "--year",
+                        type=str,
+                        dest="years",
+                        metavar="YEAR",
+                        required=True,
+                        help="The FDC year, e.g 2011-2012")
+	parser.add_argument("-r", "--round",
+                        type=int,
+                        dest="round",
+                        metavar="ROUND",
+                        required=True,
+                        help="The FDC round, i.e. 1 or 2")
+	parser.add_argument("-q", "--quarter",
+                        type=int,
+                        dest="quarter",
+                        metavar="QUARTER",
+                        required=True,
+                        help="The reporting quarter")	
 	args = parser.parse_args()
-	metrics = CommonsCatMetrics(args.category)
+	years = [int(y) for y in args.years.split('-')]
+	fdc_round = fdc.Round(years[0], years[1], args.round)
+	metrics = CommonsCatMetrics(args.category, fdc_round, quarter)
 	print "nb uploaders: %d\nnb files: %d\nnb featured content: %d" % (metrics.get_nb_uploaders(T1, T2), metrics.get_nb_files(T1, T2), 	metrics.get_nb_featured_files(T1, T2))
 	#metrics.glamorous()
 	metrics.close()
