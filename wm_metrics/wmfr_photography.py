f56b6791d0fa81185229f63a55b7557edd9dca69
PierreSelim <pierre-selim.huard@wikimedia.fr>
2014-07-23 23:16:23 +0200

    more template

--- wm_metrics/wmfr_photography.py
+++ wm_metrics/wmfr_photography.py
@@ -5,14 +5,23 @@ import MySQLdb
 
 # Output template
 template_photo = """
+== Indicateur avec pourcentages ==
 {{Suivi FDC/En-tête}}
 {{Suivi FDC/Groupe|groupe=Soutien aux photographes}}
 {{Suivi FDC/Indicateur|indicateur=Nombre de fichiers mis en ligne|q1= ${nb_q1} |q2= ${nb_q2} |q3= ${nb_q3} |q4= ${nb_q4} |cumul= ${nb_value}|Objectif=9000}}
 {{Suivi FDC/Indicateur|indicateur=% de fichiers mis en ligne et ayant reçu un label|q1=${featured_q1}|q2=${featured_q2}|q3=${featured_q3}|q4=${featured_q4}|cumul=${featured_value}|Objectif=5%}}
 {{Suivi FDC/Indicateur|indicateur=Nombre d'utilisateurs|q1=${uploaders_q1}|q2=${uploaders_q2}|q3=${uploaders_q3}|q4=${uploaders_q4}|cumul=${uploaders_value}|Objectif=80}}
 {{Suivi FDC/Fin}}
-"""
 
+== Indicateurs sans pourcentages ==
+
+{{Suivi FDC/En-tête}}
+{{Suivi FDC/Groupe|groupe=Soutien aux photographes}}
+{{Suivi FDC/Indicateur|indicateur=Nombre de fichiers mis en ligne|q1= ${nb_q1} |q2= ${nb_q2} |q3= ${nb_q3} |q4= ${nb_q4} |cumul= ${nb_value}|Objectif=9000}}
+{{Suivi FDC/Indicateur|indicateur=Bombre de fichiers mis en ligne et ayant reçu un label|q1=${nb_featured_q1}|q2=${nb_featured_q2}|q3=${nb_featured_q3}|q4=${nb_featured_q4}|cumul=${nb_featured_value}|Objectif=5%}}
+{{Suivi FDC/Indicateur|indicateur=Nombre d'utilisateurs|q1=${uploaders_q1}|q2=${uploaders_q2}|q3=${uploaders_q3}|q4=${uploaders_q4}|cumul=${uploaders_value}|Objectif=80}}
+{{Suivi FDC/Fin}}
+"""
 
 def make_example_report(fdc_round, category):
     """Quick report maker"""
@@ -38,6 +47,11 @@ def make_example_report(fdc_round, category):
                                q1=round(labels[0], 2), q2=round(labels[1], 2),
                                q3=round(labels[2], 2), q4=round(labels[3], 2),
                                value=round((labels[0]*files[0]+labels[1]*files[1]+labels[2]*files[2]+labels[3]*files[3])/(nb_file.values['value']), 2))
+    nb_labels = fdc.Indicator("nb_featured",
+                               q1=int(round(labels[0]*files[0])), q2=int(round(labels[1]*files[1])),
+                               q3=int(round(labels[2]*files[2])), q4=int(round(labels[3]*files[3])),
+                               value=int(round(labels[0]*files[0]+labels[1]*files[1]+labels[2]*files[2]+labels[3]*files[3]))
+    	)
     nb_uploader = fdc.Indicator("uploaders",
     							q1=uploaders[0],
     							q2=uploaders[1],
