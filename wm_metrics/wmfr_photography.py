bb5d18d13b973be7ae6b10499f42007af9a1c7e7
PierreSelim <pierre-selim.huard@wikimedia.fr>
2014-07-23 23:18:31 +0200

    more template

--- wm_metrics/wmfr_photography.py
+++ wm_metrics/wmfr_photography.py
@@ -18,7 +18,7 @@ template_photo = """
 {{Suivi FDC/En-tête}}
 {{Suivi FDC/Groupe|groupe=Soutien aux photographes}}
 {{Suivi FDC/Indicateur|indicateur=Nombre de fichiers mis en ligne|q1= ${nb_q1} |q2= ${nb_q2} |q3= ${nb_q3} |q4= ${nb_q4} |cumul= ${nb_value}|Objectif=9000}}
-{{Suivi FDC/Indicateur|indicateur=Bombre de fichiers mis en ligne et ayant reçu un label|q1=${nb_featured_q1}|q2=${nb_featured_q2}|q3=${nb_featured_q3}|q4=${nb_featured_q4}|cumul=${nb_featured_value}|Objectif=5%}}
+{{Suivi FDC/Indicateur|indicateur=Nombre de fichiers mis en ligne et ayant reçu un label|q1=${nb_featured_q1}|q2=${nb_featured_q2}|q3=${nb_featured_q3}|q4=${nb_featured_q4}|cumul=${nb_featured_value}|Objectif=450}}
 {{Suivi FDC/Indicateur|indicateur=Nombre d'utilisateurs|q1=${uploaders_q1}|q2=${uploaders_q2}|q3=${uploaders_q3}|q4=${uploaders_q4}|cumul=${uploaders_value}|Objectif=80}}
 {{Suivi FDC/Fin}}
 """
@@ -59,7 +59,7 @@ def make_example_report(fdc_round, category):
     							q4=uploaders[3],
     							value=uploaders[4]
     	)
-    report = fdc.Report([nb_file, pct_labels, nb_uploader], template_string=template_photo)
+    report = fdc.Report([nb_file, pct_labels, nb_uploader, nb_labels], template_string=template_photo)
     report.generate()
 
     # Ending mysql
