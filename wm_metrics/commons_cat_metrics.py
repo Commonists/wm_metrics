c4f6e39ac1f520b510d5baebec63362cc7291059
PierreSelim <pierre-selim.huard@wikimedia.fr>
2014-07-26 20:16:57 +0200

    fixing indent

--- wm_metrics/commons_cat_metrics.py
+++ wm_metrics/commons_cat_metrics.py
@@ -39,45 +39,45 @@ class Indicators:
 		# All quarters
 		self.category = category
 		self.cursor = cursor
-    		self.quarters = [CommonsCatMetrics(category, fdc_round, i + 1, cursor=cursor)
-        	              for i in range(4)]
-    		self.nb_files = None
-    		self.nb_labels = None
-    		self.nb_uploaders = None
+		self.quarters = [CommonsCatMetrics(category, fdc_round, i + 1, cursor=cursor)
+					  for i in range(4)]
+		self.nb_files = None
+		self.nb_labels = None
+		self.nb_uploaders = None
 
 	def nb_files_indicator(self, name):
-    		""" Returns an FDC indicator with count of files uploaded during q1, q2, q3, q4 and total (value). 
-		
-    		Args:
-    			name (str): name of the fdc.Indicator
-		
-    		Returns:
-    			fdc.Indicator with the number of file for each quarter and total over the whole period
-    		"""
-    		if self.nb_files == None:
-    			self.nb_files = [self.quarters[i].get_nb_files() for i in range(4)]
-    		return fdc.Indicator(name,
-    			q1=self.nb_files[0],
-    			q2=self.nb_files[1],
-    			q3=self.nb_files[2],
-    			q4=self.nb_files[3],
-    			value=sum(self.nb_files))
+		""" Returns an FDC indicator with count of files uploaded during q1, q2, q3, q4 and total (value). 
+	
+		Args:
+			name (str): name of the fdc.Indicator
+	
+		Returns:
+			fdc.Indicator with the number of file for each quarter and total over the whole period
+		"""
+		if self.nb_files == None:
+			self.nb_files = [self.quarters[i].get_nb_files() for i in range(4)]
+		return fdc.Indicator(name,
+			q1=self.nb_files[0],
+			q2=self.nb_files[1],
+			q3=self.nb_files[2],
+			q4=self.nb_files[3],
+			value=sum(self.nb_files))
 
-    	def nb_labels_indicator(self, name):
-    		""" Returns an FDC indicator with count of files uploaded during q1, q2, q3, q4 and total and which
-    		are either FP, QI or VI.
+	def nb_labels_indicator(self, name):
+		""" Returns an FDC indicator with count of files uploaded during q1, q2, q3, q4 and total and which
+		are either FP, QI or VI.
 
-    		Args:
-    			name (str): name of the fdc.Indicator
-    		"""
-    		if self.nb_labels == None:
-    			self.nb_labels = [quarters[i].get_nb_featured_files()) for i in range(4)]
-			return fdc.Indicator(name, 
-				q1=self.nb_labels[0],
-				q2=self.nb_labels[1],
-				q3=self.nb_labels[2],
-				q4=self.nb_labels[3],
-				value=sum(self.nb_labels))
+		Args:
+			name (str): name of the fdc.Indicator
+		"""
+		if self.nb_labels == None:
+			self.nb_labels = [quarters[i].get_nb_featured_files()) for i in range(4)]
+		return fdc.Indicator(name, 
+			q1=self.nb_labels[0],
+			q2=self.nb_labels[1],
+			q3=self.nb_labels[2],
+			q4=self.nb_labels[3],
+			value=sum(self.nb_labels))
 
 	def pct_labels_indicator(self, name):
 	 	""" Returns an FDC indicator with percentage of files uploaded during q1, q2, q3, q4 and total and which
@@ -98,16 +98,16 @@ class Indicators:
 				value=round(100*float(sum(self.nb_labels))/sum(self.nb_files), 2))
 
 	def nb_uploaders_indicator(self, name):
-    		""" Returns an FDC indicator with count of uploaders that have uploaded during q1, q2, q3, q4 and total (value). 
-		
-    		Args:
-    			name (str): name of the fdc.Indicator
+		""" Returns an FDC indicator with count of uploaders that have uploaded during q1, q2, q3, q4 and total (value). 
+	
+		Args:
+			name (str): name of the fdc.Indicator
 
-    		Returns:
-    			fdc.Indicator with the number of file for each quarter and total over the whole period
-    		"""
-    		if self.nb_uploaders==None:
-			self.nb_uploaders =  [self.quarters[i].get_nb_uploaders() for i in range(4)]
+		Returns:
+			fdc.Indicator with the number of file for each quarter and total over the whole period
+		"""
+		if self.nb_uploaders==None:
+		self.nb_uploaders =  [self.quarters[i].get_nb_uploaders() for i in range(4)]
 
 		cat = self.category.replace(" ", "_")
 		query = wmflabs_queries.count_uploaders_in_category(cat, fdc_round.full_period()['start'] , fdc_round.full_period()['end'])
