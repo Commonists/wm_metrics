d993d7230e9037ef75b76d3dcde6d4cc8a57d7d6
Jean-Fred <JeanFred@github>
2014-07-27 17:07:52 +0200

    Webapp: Add page for FDC reporting

--- app/wmmetrics.py
+++ app/wmmetrics.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/fdc")
+def fdc_report_page():
+    return render_template('fdc-report.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
