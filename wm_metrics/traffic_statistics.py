bdc50a4729fa42a3a7e759e4452700941c59f6bf
PierreSelim <pierre-selim.huard@wikimedia.fr>
2014-07-27 23:40:22 +0200

    Fetching month stats.

--- wm_metrics/traffic_statistics.py
+++ wm_metrics/traffic_statistics.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 
+import json
+import urllib
 
 class Traffic:
 
@@ -13,3 +15,7 @@ class Traffic:
     def _make_url(self, year, month):
         """Make the URL to the JSON output of stats.grok.se service."""
         return "http://stats.grok.se/json/{0:s}/{1:d}{2:02d}/{3:s}".format(self.site, year, month, self.title)
+
+    def get_month_traffic(self, year, month):
+        """Fetch the month traffic statistics."""
+        return json.loads(urllib.urlopen(self._make_url(year, month)).read())
