dd8abafdd00f79cf65680bc6da5d82dba16178c0
Caroline Becker <caroline.becker@wikimedia.fr>
2014-08-07 11:41:51 +0200

    typo

--- wm_metrics/category_induced.py
+++ wm_metrics/category_induced.py
@@ -81,7 +81,7 @@ class CategoryInduced:
     def list_images(self):
         import os.path
         cache_name = "cache/%s.cache" % (self.category)
-		lastContinue = ""
+        lastContinue = ""
         pros = {
                     "list"         : "categorymembers",
                     "cmtitle"      : self.category,
