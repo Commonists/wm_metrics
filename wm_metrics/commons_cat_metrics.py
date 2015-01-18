19e0b7c39d1bec2a4a7a81e3c7d85dd0d59356b8
Jean-Fred <JeanFred@github>
2015-01-18 17:52:39 +0000

    Autopep8 on commons_cat_metrics

--- wm_metrics/commons_cat_metrics.py
+++ wm_metrics/commons_cat_metrics.py
@@ -41,7 +41,7 @@ class Indicators:
         self.cursor = cursor
         self.fdc_round = fdc_round
         self.quarters = [CommonsCatMetrics(category, fdc_round, i + 1, cursor=cursor)
-                      for i in range(4)]
+                         for i in range(4)]
         self.nb_files = None
         self.nb_labels = None
         self.nb_uploaders = None
@@ -49,21 +49,21 @@ class Indicators:
 
     def nb_files_indicator(self, name):
         """ Returns an FDC indicator with count of files uploaded during q1, q2, q3, q4 and total (value). 
-    
+
         Args:
             name (str): name of the fdc.Indicator
-    
+
         Returns:
             fdc.Indicator with the number of file for each quarter and total over the whole period
         """
         if self.nb_files == None:
             self.nb_files = [self.quarters[i].get_nb_files() for i in range(4)]
         return fdc.Indicator(name,
-            q1=self.nb_files[0],
-            q2=self.nb_files[1],
-            q3=self.nb_files[2],
-            q4=self.nb_files[3],
-            value=sum(self.nb_files))
+                             q1=self.nb_files[0],
+                             q2=self.nb_files[1],
+                             q3=self.nb_files[2],
+                             q4=self.nb_files[3],
+                             value=sum(self.nb_files))
 
     def nb_labels_indicator(self, name):
         """ Returns an FDC indicator with count of files uploaded during q1, q2, q3, q4 and total and which
@@ -73,13 +73,14 @@ class Indicators:
             name (str): name of the fdc.Indicator
         """
         if self.nb_labels == None:
-            self.nb_labels = [self.quarters[i].get_nb_featured_files() for i in range(4)]
-        return fdc.Indicator(name, 
-            q1=self.nb_labels[0],
-            q2=self.nb_labels[1],
-            q3=self.nb_labels[2],
-            q4=self.nb_labels[3],
-            value=sum(self.nb_labels))
+            self.nb_labels = [
+                self.quarters[i].get_nb_featured_files() for i in range(4)]
+        return fdc.Indicator(name,
+                             q1=self.nb_labels[0],
+                             q2=self.nb_labels[1],
+                             q3=self.nb_labels[2],
+                             q4=self.nb_labels[3],
+                             value=sum(self.nb_labels))
 
     def __percent(self, a, b, decimals=2):
         """ Percentage of a/b rounded with decimals. If b==0 it returns None.
@@ -92,56 +93,63 @@ class Indicators:
         Returns:
             100*a/b 
         """
-        if b==0:
+        if b == 0:
             return None
         else:
-            return round(100*float(a)/b, decimals)
+            return round(100 * float(a) / b, decimals)
 
     def pct_labels_indicator(self, name):
         """ Returns an FDC indicator with percentage of files uploaded during q1, q2, q3, q4 and total and which
             are either FP, QI or VI.
-        
+
             Args:
                 name (str): name of the fdc.Indicator
         """
-        if self.nb_files==None:
+        if self.nb_files == None:
             self.nb_files = [self.quarters[i].get_nb_files() for i in range(4)]
-        if self.nb_labels==None:
-            self.nb_labels = [self.quarters[i].get_nb_featured_files() for i in range(4)]
+        if self.nb_labels == None:
+            self.nb_labels = [
+                self.quarters[i].get_nb_featured_files() for i in range(4)]
         return fdc.Indicator(name,
-                q1=self.__percent(self.nb_labels[0], self.nb_files[0]),
-                q2=self.__percent(self.nb_labels[1], self.nb_files[1]),
-                q3=self.__percent(self.nb_labels[2], self.nb_files[2]),
-                q4=self.__percent(self.nb_labels[3], self.nb_files[3]),
-                value=self.__percent(sum(self.nb_labels),sum(self.nb_files)))
+                             q1=self.__percent(
+                                 self.nb_labels[0], self.nb_files[0]),
+                             q2=self.__percent(
+                                 self.nb_labels[1], self.nb_files[1]),
+                             q3=self.__percent(
+                                 self.nb_labels[2], self.nb_files[2]),
+                             q4=self.__percent(
+                                 self.nb_labels[3], self.nb_files[3]),
+                             value=self.__percent(sum(self.nb_labels), sum(self.nb_files)))
 
     def nb_uploaders_indicator(self, name):
         """ Returns an FDC indicator with count of uploaders that have uploaded during q1, q2, q3, q4 and total (value). 
-    
+
         Args:
             name (str): name of the fdc.Indicator
 
         Returns:
             fdc.Indicator with the number of file for each quarter and total over the whole period
         """
-        if self.nb_uploaders==None:
-            self.nb_uploaders =  [self.quarters[i].get_nb_uploaders() for i in range(4)]
+        if self.nb_uploaders == None:
+            self.nb_uploaders = [
+                self.quarters[i].get_nb_uploaders() for i in range(4)]
 
         cat = self.category.replace(" ", "_")
-        query = wmflabs_queries.count_uploaders_in_category(cat, self.fdc_round.full_period()['start'] , self.fdc_round.full_period()['end'])
+        query = wmflabs_queries.count_uploaders_in_category(
+            cat, self.fdc_round.full_period()['start'], self.fdc_round.full_period()['end'])
         self.cursor.execute(query)
         total = long(self.cursor.fetchone()[0])
 
         return fdc.Indicator(name,
-                q1 = self.nb_uploaders[0],
-                q2 = self.nb_uploaders[1],
-                q3 = self.nb_uploaders[2],
-                q4 = self.nb_uploaders[3],
-                value = total)
+                             q1=self.nb_uploaders[0],
+                             q2=self.nb_uploaders[1],
+                             q3=self.nb_uploaders[2],
+                             q4=self.nb_uploaders[3],
+                             value=total)
 
     def total_usage_indicator(self, name):
         """ Returns an FDC indicator with count of usages of image from the category.
-        
+
         Args:
             name (str): name of the fdc.Indicator
         """
@@ -157,7 +165,7 @@ class Indicators:
         """
         if self.global_usage == None:
             self.global_usage = self.quarters[0].get_global_usage()
-        return fdc.Indicator(name, value=self.global_usage['images used'])        
+        return fdc.Indicator(name, value=self.global_usage['images used'])
 
     def nb_wiki_indicator(self, name):
         """ Returns an FDC indicator with count of wiki using an image from the category. 
@@ -167,11 +175,13 @@ class Indicators:
         """
         if self.global_usage == None:
             self.global_usage = self.quarters[0].get_global_usage()
-        return fdc.Indicator(name, value=self.global_usage['nb wiki'])   
+        return fdc.Indicator(name, value=self.global_usage['nb wiki'])
 
 
 class CommonsCatMetrics:
+
     """Wrapper class for the Category Metrics"""
+
     def __init__(self, category, round_fdc, q, cursor=None):
         """Constructor
 
@@ -183,12 +193,12 @@ class CommonsCatMetrics:
         """
         self.catsql = category.replace(" ", "_")
 
-        if cursor==None:
+        if cursor == None:
             db = get_commons_db()
             self.cursor = db.cursor()
         else:
             self.cursor = cursor
-        
+
         # round fdc
         timestamps = round_fdc.quarter(q)
         self.timestamp1 = timestamps['start']
@@ -196,19 +206,22 @@ class CommonsCatMetrics:
 
     def get_nb_uploaders(self):
         """Amount of uploaders on the period."""
-        query = wmflabs_queries.count_uploaders_in_category(self.catsql, self.timestamp1, self.timestamp2)
+        query = wmflabs_queries.count_uploaders_in_category(
+            self.catsql, self.timestamp1, self.timestamp2)
         self.cursor.execute(query)
         return long(self.cursor.fetchone()[0])
 
     def get_nb_files(self):
         """Amount of files uploaded on the period."""
-        query = wmflabs_queries.count_files_in_category(self.catsql, self.timestamp1, self.timestamp2)
+        query = wmflabs_queries.count_files_in_category(
+            self.catsql, self.timestamp1, self.timestamp2)
         self.cursor.execute(query)
         return long(self.cursor.fetchone()[0])
 
     def get_nb_featured_files(self):
         """Amount of files that are either FP, VI or QI on Wikimedia Commons."""
-        query = wmflabs_queries.count_featured_files_in_category(self.catsql, self.timestamp1, self.timestamp2)
+        query = wmflabs_queries.count_featured_files_in_category(
+            self.catsql, self.timestamp1, self.timestamp2)
         self.cursor.execute(query)
         return long(self.cursor.fetchone()[0])
 
@@ -225,7 +238,7 @@ class CommonsCatMetrics:
         return {
             'total usage': long(result[0]),
             'images used': long(result[1]),
-            'nb wiki': long(result[2]) }
+            'nb wiki': long(result[2])}
 
     def get_nb_files_alltime(self):
         """ Returns nb of files in category."""
@@ -282,7 +295,7 @@ def main():
     print "global usage(as of now):"
     print "\tnb files: %d" % nb_files
     print "\ttotal usages: %d" % global_usage['total usage']
-    print "\timages in use: %d (%.2f %%)" % (global_usage['images used'], 100.*float(global_usage['images used'])/nb_files)
+    print "\timages in use: %d (%.2f %%)" % (global_usage['images used'], 100. * float(global_usage['images used']) / nb_files)
     print "\tnb wiki: %d" % global_usage['nb wiki']
 
     # closing SQL connection
