5f24ae557d2351a6b46d89680be3e5e4527c5a2d
PierreSelim <pierre-selim.huard@wikimedia.fr>
2015-06-29 21:01:55 +0200

    Style glamorous.py
    Added docstring, and put URL of glamorous API as global variable of the module.

--- wm_metrics/glamorous.py
+++ wm_metrics/glamorous.py
@@ -7,12 +7,20 @@ from HTMLParser import HTMLParser
 from argparse import ArgumentParser
 import urllib
 
+GLAMOROUS_URL = "http://tools.wmflabs.org/glamtools/glamorous.php"
+GLAMOROUS_QUERY = "?doit=1&category=%s&use_globalusage=1&ns0=1"
+
 
 class GlamorousParser(HTMLParser, object):
 
     """HTML parser glamorous"""
 
     def __init__(self, category):
+        """Constructor.
+
+        Args:
+            category (str): Category name.
+        """
         super(GlamorousParser, self).__init__()
         self.category = category
         self.page = urllib.urlopen(self.__category_url__()).read()
@@ -22,6 +30,7 @@ class GlamorousParser(HTMLParser, object):
         self.feed(self.page)
 
     def statistics(self):
+        """Print GLAMorous statistics for the category."""
         nb_wiki = len(self.result.keys()) - 2
         images_usages = int(self.result['Total image usages'])
         images_used = int(self.result['Distinct images used'])
@@ -29,13 +38,11 @@ class GlamorousParser(HTMLParser, object):
             (nb_wiki, images_usages, images_used)
 
     def __category_url__(self):
-        """
-        Return the URL to glamorous of the category
-        """
-        return "http://tools.wmflabs.org/glamtools/glamorous.php?doit=1&category=%s&use_globalusage=1&ns0=1" % \
-            self.category
+        """Return the URL to glamorous of the category"""
+        return (GLAMOROUS_QUERY + GLAMOROUS_URL) % self.category
 
     def handle_starttag(self, tag, attrs):
+        """Parse start of an HTML tag."""
         if tag == 'th' and self.status == 0:
             self.status = 1
         if tag == 'th' and self.status == 3:
@@ -44,12 +51,14 @@ class GlamorousParser(HTMLParser, object):
             self.status = 5
 
     def handle_endtag(self, tag):
+        """Parse end of an HTML tag."""
         if tag == 'th' and self.status == 1:
             self.status = 0
         if tag == 'td' and self.status == 5:
             self.status = 3
 
     def handle_data(self, data):
+        """Parse data inside an HTML tag."""
         if data.strip() == 'Images used':
             self.status = 3
         if self.status == 4:
