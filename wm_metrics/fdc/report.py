da4d89e9ebfcb3abf8d999cfb28059089f7a1030
PierreSelim <pierre-selim.huard@wikimedia.fr>
2015-06-30 00:39:14 +0200

    Improved docstring style.

--- wm_metrics/fdc/report.py
+++ wm_metrics/fdc/report.py
@@ -7,20 +7,18 @@ from string import Template
 
 class Report(object):
 
-    """Report is build from indicator list and template
-    and generate a report.
-    """
+    """Built from indicator list and template and generate a report."""
 
     def __init__(self, indicator_list, template_string=None, template_file=None):
-        """ Initialize a report.
+        """Initialize a report.
 
         In template variables are named:
                 $name_q1, $name_q2 $name_q3, $name_q4, $name_value
 
         Args:
-                indicator_list (list): list of fdc.Indicator
-                template_string (str): Template string of the report to use (iff there is no template_file)
-                template_file (file): File containing the template string to use to generate the report
+            indicator_list (list): list of fdc.Indicator
+            template_string (str): Template string of the report to use (iff there is no template_file)
+            template_file (file): File containing the template string to use to generate the report
         """
         self.template = Template(template_string)
         self.indicator_values = dict()
@@ -32,10 +30,12 @@ class Report(object):
         self._fill_indicator_contents(indicator_list)
 
     def _read_template_file(self, template_file):
+        """Read template file."""
         with open(template_file, 'r') as f:
             return Template(f.read())
 
     def _fill_indicator_contents(self, indicator_list):
+        """Fill the indicator values dictionary."""
         for indicator in indicator_list:
             for key in indicator.values.keys():
                 var_name = "%s_%s" % (indicator.name, key)
@@ -45,6 +45,5 @@ class Report(object):
                     self.indicator_values[var_name] = indicator.values[key]
 
     def generate(self):
-        """ Generate report from template and indicators.
-        """
+        """Generate report from template and indicators."""
         return self.template.safe_substitute(self.indicator_values)
